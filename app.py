eb4699b05ec2fa10900ab32aed64b655ab5de154
pcdivakar <pcdivakar@gmail.com>
2023-09-27 22:16:10 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import cv2
 from deepface import DeepFace
+import os
 
 app = Flask(__name__)
 
@@ -53,5 +54,7 @@ cap.release()
 cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
